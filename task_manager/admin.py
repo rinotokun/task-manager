fa43429dafc4fd52633f7c08742e59de923f9e18
Sergey <rinoto_kun@gmail.com>
2026-07-23 12:11:37 +0300

    Register models in the admin panel
    Added the Task, TaskType, Position, and Worker models to the admin panel.
    
    The Group model has been unregistered from the admin panel, because permissions are not managed through groups in this project.

--- task_manager/admin.py
+++ task_manager/admin.py
@@ -1,3 +1,50 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import Group
 
-# Register your models here.
+from .models import (
+    Worker,
+    Position,
+    Task,
+    TaskType,
+)
+
+
+@admin.register(Worker)
+class WorkerAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ("position",)
+    fieldsets = UserAdmin.fieldsets + (
+        (("Additional info", {"fields": ("position",)}),)
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (
+            (
+                "Additional info",
+                {
+                    "fields": (
+                        "first_name",
+                        "last_name",
+                        "position",
+                    )
+                },
+            ),
+        )
+    )
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "description",
+        "deadline",
+        "is_completed",
+        "priority",
+        "task_type",
+    )
+    list_filter = ("is_completed", "priority", "deadline",)
+
+
+admin.site.register(Position)
+admin.site.register(TaskType)
+admin.site.unregister(Group)
