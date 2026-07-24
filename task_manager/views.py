de66fd7d69da59a526183d134aa609c561772050
Sergey <rinoto_kun@gmail.com>
2026-07-24 12:17:37 +0300

    Show task counters on the index page
    The index view now passes the total number of tasks in the system and the number of tasks assigned to the current user, displayed as two cards on the page.

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
 
+from .models import Task
+
 
 def index(request):
-    return render(request, "task_manager/index.html")
+    num_user_tasks = request.user.tasks.count()
+    num_tasks = Task.objects.count()
+    context = {
+        "num_user_tasks": num_user_tasks,
+        "num_tasks": num_tasks,
+    }
+    return render(
+        request,
+        "task_manager/index.html",
+        context=context
+    )
