0d1b4b1ad2a9de7bfb5df7cff5666286ba4462b9
Sergey <rinoto_kun@gmail.com>
2026-07-25 03:15:48 +0300

    Add task detail, create, update and delete pages

--- task_manager/views.py
+++ task_manager/views.py
@@ -5,6 +5,7 @@ from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .models import Task, TaskType, Position
+from .forms import TaskForm
 
 
 @login_required
@@ -31,6 +32,55 @@ class TaskListView(LoginRequiredMixin, generic.ListView):
             "task_type"
         ).prefetch_related("assignees")
         return queryset
+
+
+class TaskDetailView(LoginRequiredMixin, generic.DetailView):
+    model = Task
+
+    def get_queryset(self):
+        queryset = Task.objects.select_related(
+            "task_type"
+        ).prefetch_related("assignees")
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["back_url"] = self.request.GET.get(
+            "next",
+            reverse_lazy("task_manager:task-list")
+        )
+        return context
+
+
+class TaskCreateView(LoginRequiredMixin, generic.CreateView):
+    model = Task
+    form_class = TaskForm
+    success_url = reverse_lazy("task_manager:task-list")
+
+
+class TaskUpdateView(LoginRequiredMixin, generic.UpdateView):
+    model = Task
+    form_class = TaskForm
+
+    def get_success_url(self):
+        current_task_id = self.object.pk
+        return reverse_lazy(
+            "task_manager:task-detail",
+            kwargs={"pk": current_task_id}
+        )
+
+
+class TaskDeleteView(LoginRequiredMixin, generic.DeleteView):
+    model = Task
+    success_url = reverse_lazy("task_manager:task-list")
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["cancel_url"] = self.request.GET.get(
+            "next",
+            reverse_lazy("task_manager:task-list")
+        )
+        return context
 
 
 class TaskTypeListView(LoginRequiredMixin, generic.ListView):
