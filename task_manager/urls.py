e135a0de1e36c02245ac80e6e133bdfa8bab99f8
Sergey <rinoto_kun@gmail.com>
2026-07-25 03:15:48 +0300

    Add task detail, create, update and delete pages

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -3,6 +3,10 @@ from django.urls import path
 from .views import (
     index,
     TaskListView,
+    TaskDetailView,
+    TaskCreateView,
+    TaskUpdateView,
+    TaskDeleteView,
     TaskTypeListView,
     TaskTypeCreateView,
     TaskTypeDeleteView,
@@ -15,6 +19,26 @@ from .views import (
 urlpatterns = [
     path("", index, name="index"),
     path("tasks/", TaskListView.as_view(), name="task-list"),
+    path(
+        "tasks/create/",
+        TaskCreateView.as_view(),
+        name="task-create"
+    ),
+    path(
+        "tasks/<int:pk>/update/",
+        TaskUpdateView.as_view(),
+        name="task-update"
+    ),
+    path(
+        "tasks/<int:pk>/detail/",
+        TaskDetailView.as_view(),
+        name="task-detail"
+    ),
+    path(
+        "tasks/<int:pk>/delete/",
+        TaskDeleteView.as_view(),
+        name="task-delete"
+    ),
     path("task-types/", TaskTypeListView.as_view(), name="tasktype-list"),
     path(
         "task-types/create/",
